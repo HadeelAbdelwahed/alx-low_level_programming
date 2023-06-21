baa835d85c2e2e3857573b9bfceacaea64524712
HadeelAbdelwahed <hadeelabdewahed@outlook.com>
2023-06-20 19:51:23 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 
-/**
- * _putchar - writes a character to the standard output
- * @c: the character to be written
- *
- * Return: On success, the character written is returned.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+int main() {
+    putchar('H');
+    putchar('e');
+    putchar('l');
+    putchar('l');
+    putchar('o');
+    putchar(',');
+    putchar(' ');
+    putchar('W');
+    putchar('o');
+    putchar('r');
+    putchar('l');
+    putchar('d');
+    putchar('!');
+    putchar('\n');
 
-int main(void)
-{
-    printf("_putchar\n");
     return 0;
 }
